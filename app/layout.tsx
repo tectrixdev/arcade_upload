56d43c6ccf327094feeaf188fcf92da68c4da5ef
TecTrix <joranhennion@duck.com>
2025-04-02 19:56:23 +0200

    add analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import { Analytics } from "@vercel/analytics/react";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
@@ -30,6 +31,7 @@ export default function RootLayout({
       <body className="w-screen h-screen overflow-hidden antialiased bg-black">
         {children}
       </body>
+      <Analytics />
     </html>
   );
 }
