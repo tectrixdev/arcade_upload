8ecddeaf22856e8bd43ed4fa69e2f05ea9407953
TecTrix <joranhennion@duck.com>
2025-04-02 23:00:59 +0200

    better api lmao, realized I was stupid after reading a nextjs official tutorial on it lmao

---
+++ app/api/latesturl/route.ts
@@ -0,0 +1,18 @@
+"use server";
+import { neon } from "@neondatabase/serverless";
+import type { NextApiRequest, NextApiResponse } from "next";
+
+type ResponseData = {
+  message: string;
+};
+
+const sql = neon(`${process.env.DATABASE_URL}`);
+const rows = await sql`SELECT url FROM url ORDER BY id DESC LIMIT 1`;
+const response = rows[0]?.url || null;
+
+export async function GET(request: Request) {
+  return new Response(JSON.stringify(response), {
+    status: 200,
+    headers: { "Content-Type": "application/json" },
+  });
+}
