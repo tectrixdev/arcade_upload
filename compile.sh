39b83420bf6bc4e08b424b7c3b03479b86541b2c
Joran Hennion <joranhennion@duck.com>
2025-04-26 11:54:40 +0200

    add compiling script

---
+++ compile.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# download mkc first, https://github.com/microsoft/pxt-mkc
+# pnpm i -g makecode
+# this script downloads and compiles the latest project
+# the if statement is to allow updating projects while also not building duplicates
+
+if [ $(pnpm list -g | grep makecode -o) = "makecode" ]; then
+    echo "skipping dependencies because they seem already present"
+else
+    echo "installing the dependencies"
+    pnpm setup
+    source ~/.bashrc
+    pnpm i -g makecode -s
+fi
+
+echo "setting up filetree"
+rm -r /tmp/arcade/
+mkdir /tmp/arcade/
+cd /tmp/arcade/
+echo "downloading files"
+mkc download $1
+echo "building project"
+mkc build -f rawELF --hw rpi
+mv built/rpi/binary.elf ~/$(cat /tmp/arcade/pxt.json | jq -r '.name' | tr -cd '[:alnum:] ' | tr ' ' '_').elf
+echo "finishing"
+echo "done"
